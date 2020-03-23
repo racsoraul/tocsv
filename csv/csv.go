deffa3f3a55a6eaa789ad60a3cf0e7f5445e7af4
Raúl Pleitez <racsoraul@gmail.com>
2020-03-22 18:26:42 -0600

    implements ProcessLines. All tests passing

--- csv/csv.go
+++ csv/csv.go
@@ -1,13 +1,52 @@
 package csv
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"regexp"
+	"strings"
 )
 
 // ProcessLines processes each line with values separated by spaces
 // and turns it into a comma separated line (csv like).
 func ProcessLines(line string, isHeader bool) (string, error) {
-	return "", nil
+	line = strings.TrimSpace(line)
+
+	r, err := regexp.Compile("\\s+")
+
+	if err != nil {
+		return "", err
+	}
+
+	separatedByOneSpace := r.ReplaceAllString(line, " ")
+	elements := strings.Split(separatedByOneSpace, " ")
+
+	if len(elements[0]) > 0 {
+		switch len(elements) {
+		case 1:
+			return elements[0] + ",,", nil
+		case 2:
+			switch len(elements[1]) {
+			case 14:
+				return elements[0] + "," + elements[1] + ",", nil
+			case 9:
+				return elements[0] + ",," + elements[1], nil
+			}
+			return "", fmt.Errorf("Element with %d characters can't be identified as NIT nor DUI", len(elements[1]))
+		case 3:
+			if !isHeader && elements[1] != "NULL" && len(elements[1]) != 14 {
+				return "", fmt.Errorf("Element with %d characters can't be identified as NIT", len(elements[1]))
+			} else if !isHeader && elements[2] != "NULL" && len(elements[2]) != 9 {
+				return "", fmt.Errorf("Element with %d characters can't be identified as DUI", len(elements[2]))
+			}
+			return strings.Join(elements, ","), nil
+		default:
+			return "", errors.New("Line with more than 3 elements")
+		}
+	}
+
+	return "", errors.New("Line with no data. Correlative, NIT and DUI are missing")
 }
 
 // LineCounter returns the number of lines read by a reader
